c68a8ac0e2616ba6643e05798e3b4fb8b1eef1b4
Daniel Sloane <dsloane@catalystdevworks.com>
2016-03-22 16:07:13 -0700

    added google play library

---
+++ app/src/main/java/com/example/catalyst/androidtodo/fragments/MapFragment.java
@@ -0,0 +1,29 @@
+package com.example.catalyst.androidtodo.fragments;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+import com.google.android.gms.maps.GoogleMap;
+
+/**
+ * Created by dsloane on 3/22/2016.
+ */
+public class MapFragment extends DialogFragment {
+
+    private static final String TAG = MapFragment.class.getSimpleName();
+
+    private GoogleMap googleMap;
+
+    public MapFragment() {}
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+    }
+
+
+
+
+
+}
